5c73bfbbb86f356b9b067c7938f77572c0c15114
Monica Borra <mbn6f@mst.edu>
2017-04-29 21:28:51 -0500

    updated user

--- yelp_user.py
+++ yelp_user.py
@@ -7,12 +7,14 @@ import unicodedata
 filename = "yelp_academic_dataset_user.json"
 
 with open('users.csv', 'w') as file:
-    w = csv.writer(file)
-    w.writerow(["user_id", "year", "num_friends", "votes", "num_reviews", "avg rating"])
+
+    headers = ["user_id", "year", "num_friends", "votes", "num_reviews", "avg rating"]
+    w = csv.DictWriter(file, delimiter=',', lineterminator='\n', fieldnames=headers)
     with open(filename, encoding="utf-8") as f:
         for line in f:
             data = json.loads(line)
             votes = data['useful'] + data['funny'] + data['cool']
             print(len(data['friends']))
             if data['review_count'] >= 30:
-                w.writerow([data['user_id'], data['yelping_since'], len(data['friends']), votes, data['review_count'], data['average_stars']])
+                w.writerow({"user_id": data['user_id'], "year": data['yelping_since'], "num_friends":len(data['friends']),
+                            "votes": votes, "num_reviews": data['review_count'], "avg rating": data['average_stars']})
